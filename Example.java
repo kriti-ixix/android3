cb25229afc253d6cf9e286f1b19f6e3a97fa916e
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-02 16:34:52 +0530

    Objects & Functions

---
+++ Example.java
@@ -0,0 +1,30 @@
+class Example
+{
+    int x = 10, y = 20;
+
+    void addNumbers()
+    {
+        System.out.println(x + y);
+    }
+
+    int subNumbers()
+    {
+        return x - y;
+    }
+
+    public static void main(String[] args)
+    {
+        Example ex = new Example();
+        //System.out.println("Hello world!");
+        //System.out.println(ex.x);
+        //System.out.println(ex.y);
+        ex.addNumbers();
+        int output1 = ex.subNumbers();
+
+        Example ex2 = new Example();
+        ex2.x = 100;
+        //System.out.println(ex2.x);
+        ex2.addNumbers();
+        int output2 = ex2.subNumbers();
+    }
+}
