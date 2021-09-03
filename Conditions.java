53c4fabd6f45b9ee42dbd88eb4dd65ffcd7b99e3
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-03 15:50:14 +0530

    conditional statements

---
+++ Conditions.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Conditions
+{
+    public static void main(String[] args)
+    {
+        //int age = 19;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        if (age > 17)
+        {
+            System.out.println("You are eligible to vote");
+        }
+        else
+        {
+            System.out.println("You are not eligible to vote");
+        }
+
+    }
+}
